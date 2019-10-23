2a34712d2f9cf505c8e1c5af82c57004598d6a13
doozi316 <doozi316@gmail.com>
2019-10-23 22:14:43 +0900

    장르 출력하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component{
 
-  
+
   state = {
     isLoading : true,
     movies : []
@@ -28,20 +29,33 @@ class App extends React.Component{
   }
 
 
-  render(){
-    
-      const {isLoading, movies} = this.state;
-      return <div>{isLoading ? "Loading..." : movies.map(movie => {
-        console.log(movie);
-        return ( 
-        <Movie //Movie.js 의 props 가져오기
-        id={movie.id} 
-        year={movie.year} 
-        title={movie.title} 
-        summary={movie.summary} 
-        poster={movie.mideum_cover_image}/>
-      )})}</div>
+  render() {
+    const { isLoading, movies } = this.state;
+    return ( //react가 클래스와 속성을 혼란스러워하므로 JSX의 class는 className이라고 해주기
+      <section className="container"> 
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
+
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,23 @@
 import React from "react";
 import propTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}){
-    return <h4>{title}</h4>;
+function Movie({year, title, summary, poster, genres}){
+    return (
+    <div className="movies__movie">
+        <img src={poster} alt={title} title={title}/>
+        <div className="movie__data">
+            <h3 className="movie__title">{title}></h3>
+            <h5 className="movie__year">{year}</h5>
+            <ul className="genres">
+                {genres.map((genre, index) => (
+                    <li key={index} className="genres__genre">{genre}</li>
+                ))}
+            </ul>
+        </div>
+            <p className="movie__summary">{summary}</p>
+    </div>
+        )
 }
 
 Movie.propTypes ={
@@ -10,7 +25,8 @@ Movie.propTypes ={
     year : propTypes.number.isRequired,
     title : propTypes.string.isRequired,
     summary : propTypes.string.isRequired,
-    poster : propTypes.string.isRequired
+    poster : propTypes.string.isRequired,
+    genres : propTypes.arrayOf(propTypes.string).isRequired
 };
 
 export default Movie;
