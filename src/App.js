e84056cda114ada2e732df83a4e784b3aa9d0689
doozi316 <doozi316@gmail.com>
2019-10-23 21:43:59 +0900

    YTS API 가져온 뒤 원하는 data 출력하기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import Movie from "./Movie";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  
+  state = {
+    isLoading : true,
+    movies : []
+    };
+  
+
+    // YTS API 가져오는 메소드 getMovies
+    getMovies = async() =>{ //비동기
+      const {
+        data: {
+          data:
+          { movies } // movies.data.data.movies
+        }
+      } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating"); //axios가 완료될 때까지 기다린다.
+      this.setState({ movies, isLoading : false });
+    }
+  
+
+    componentDidMount(){
+    this.getMovies(); // 컴포넌트가 생성된 뒤, getMovies 실행
+  }
+
+
+  render(){
+    
+      const {isLoading, movies} = this.state;
+      return <div>{isLoading ? "Loading..." : movies.map(movie => {
+        console.log(movie);
+        return ( 
+        <Movie //Movie.js 의 props 가져오기
+        id={movie.id} 
+        year={movie.year} 
+        title={movie.title} 
+        summary={movie.summary} 
+        poster={movie.mideum_cover_image}/>
+      )})}</div>
+  }
 }
 
 export default App;
